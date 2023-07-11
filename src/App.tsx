99c2083618bc091a1045f0a780cdd71d782d0eae
Mike <spormuv@gmail.com>
2023-07-11 13:24:32 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Benefits from '@/scenes/benefits';
 import ContactUs from '@/scenes/contactUs';
+import Footer from '@/scenes/footer';
 import Home from '@/scenes/home';
 import Navbar from '@/scenes/navbar';
 import OurClasses from '@/scenes/ourClasses';
@@ -38,6 +39,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
